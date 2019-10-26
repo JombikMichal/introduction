112061c0c3f51a41d49b9e9a26e596a6422be7f9
Michal <mjombik@kkmbfin.sk>
2019-10-26 12:45:43 +0200

    #2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
         //#1 It would not be a proper start without Hello World! program. Just print Hello World! string to the console.
         System.out.println("Hello World!");
+        Triangle triangle = new Triangle(5);
+        triangle.print();
     }
 }
---
+++ src/main/java/Triangle.java
@@ -0,0 +1,18 @@
+public class Triangle {
+    private final int height;
+
+    public Triangle(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void print(){
+        //#2 Implement a method that will draw a triangle from * for given height h,
+        for (int i = 0; i < height; i ++){
+            System.out.println("*".repeat(i + 1));
+        }
+    }
+}
